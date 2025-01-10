964c8765ca952c36eb52b3eacc2111d525e3d574
atziallas <alex.tziallas@gmail.com>
2025-01-10 23:04:10 +0200

    corrected mistake in middleware configuration

--- django/src/tidy/settings_common.py
+++ django/src/tidy/settings_common.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'core.middleware.HealthCheckMiddleware'
+    'core.middleware.HealthCheckMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
