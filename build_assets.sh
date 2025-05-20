7571a1792f62f5e4db5ec31ba8baf83403ed50ad
atziallas <alex.tziallas@gmail.com>
2025-05-21 00:43:59 +0300

    kamal static files through nginx operational

--- build_assets.sh
+++ build_assets.sh
@@ -2,7 +2,8 @@
 docker compose up -d node
 docker compose exec node npm run build
 cp node/src/dist/* django/src/core/static/core/assets
+cp -R django/src/core/static/core nginx-static/tidy
 
-# when using a separate static site deployment
+# for when using a separate static site deployment
 # rm -r ../tziallas.net_static/tidy/*
 # cp -R django/src/core/static/ ../tziallas.net-static/static/tidy
